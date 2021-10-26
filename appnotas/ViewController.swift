a31044fbf7a17200001fd68f489d43de050ea12e
pinpon <mac6@iMac-de-Mac6.local>
2021-10-26 16:52:03 -0500

    appnotas1

---
+++ appnotas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appnotas
+//
+//  Created by Mac6 on 26/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
